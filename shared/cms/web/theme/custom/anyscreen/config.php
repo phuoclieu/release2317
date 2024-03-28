a1f0429e2f8d2b2529b7468c7dd021af0a793dc4
Phuoc Lieu <phuoclieu@hotmail.com>
2024-03-28 14:18:02 +0700

    Add files via upload

---
+++ shared/cms/web/theme/custom/anyscreen/config.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Copyright (C) 2006-2021 Xibo Signage Ltd
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+defined('XIBO') or die("Sorry, you are not allowed to directly access this page.<br /> Please press the back button in your browser.");
+
+$config = array(
+		'theme_name' => 'anyscreen',
+		'theme_title' => 'CMS',
+		'app_name' => 'Anyscreen',
+		'theme_url' => 'https://compasstech.asia/',
+		'cms_source_url' => 'https://github.com/xibosignage/xibo-cms',
+		'cms_install_url' => 'manual/en/install_cms.html',
+		'cms_release_notes_url' => 'manual/en/release_notes.html',
+		'latest_news_url' => 'http://xibo.org.uk/feed/',
+		'client_sendCurrentLayoutAsStatusUpdate_enabled' => false,
+		'client_screenShotRequestInterval_enabled' => false,
+		'view_path' => '../web/theme/custom/anyscreen/views',
+		'product_support_url' => 'https://community.xibo.org.uk/c/support'
+	);
